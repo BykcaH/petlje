97e6fc68ad93311967d08e04eded4cb8c885f296
CSU <CSU@CSU-TOSH>
2018-10-18 11:20:22 +0200

    niz inicijalizacija

--- src/Demo.java
+++ src/Demo.java
@@ -3,8 +3,13 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] niz = {12, 13, 14, 15, 17};
- 		for(int i : niz) {
+		int [] niz = new int[3];
+		niz[0] = 7;
+		niz[1] = 6;
+		niz[2] = 5;
+//		niz[3] = 4; Greska kapacitet niza je ogranizen na 3
+		
+		for(int i : niz) {
  			System.out.println(" Vrednost element niza " 
  					+ " je " + i );
  		}
