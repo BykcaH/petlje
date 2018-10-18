878e76048516ff0db648a5c805bf38e270928864
CSU <CSU@CSU-TOSH>
2018-10-18 10:58:12 +0200

    beskonacna petlja

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] niz = {12, 13, 14, 15, 17};
+//		int [] niz = {12, 13, 14, 15, 17};
  		for(; ; ) {
 			System.out.println("Beskonacna petlja");
 		}
