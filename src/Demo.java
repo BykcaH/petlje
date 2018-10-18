f3e05e25cad9583c73e5420d294e660b84e10598
CSU <CSU@CSU-TOSH>
2018-10-18 11:14:07 +0200

    foreach petlja

--- src/Demo.java
+++ src/Demo.java
@@ -4,9 +4,9 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] niz = {12, 13, 14, 15, 17};
- 		for(int i = 0; i < niz.length ; i++ ) {
- 			System.out.println("Element niza " + i 
- 					+ " je " + niz[i] );
+ 		for(int i : niz) {
+ 			System.out.println(" Vrednost element niza " 
+ 					+ " je " + i );
  		}
 	}
 
