3fc975b0d1dbed35856b2849555d937a39d62a56
CSU <CSU@CSU-TOSH>
2018-10-22 10:17:17 +0200

    Primer sa matricom

--- src/Demo.java
+++ src/Demo.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Demo {
 
 	public static void main(String[] args) {
-		char[] nizKaraktera = new char[5];
+		char[][] nizKaraktera = new char[3][3];
 		System.out.println(nizKaraktera.length);
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Unesit 5 karaktera");
 		for(int i = 0; i < nizKaraktera.length ; i++) {
-			nizKaraktera[i] = input.nextLine().charAt(0);
+			for(int j = 0; j < 3; j++) {
+				nizKaraktera[i][j] = input.nextLine().charAt(0);
+			}
 		}
 		
 		System.out.println("Unesite karakter za proveru");
@@ -17,11 +19,13 @@ public class Demo {
 		
 		boolean nalaziSeUNizu = false;
 		for(int i = 0; i < nizKaraktera.length; i++) {
-			if(nizKaraktera[i] == provera) {
+			for(int j = 0; j < 3; j++) {
+			if(nizKaraktera[i][j] == provera) {
 				System.out.println("Char " + provera +
-						" se nalazi na " +  (i+1) + " poziciji");
+						" se nalazi na " +  (i+1) + "," + (j+1) + " poziciji");
 				nalaziSeUNizu = true;
 			} 
+		}
 		}
 		if(nalaziSeUNizu == false) {
 			System.out.println("Element se ne nalazi u nizu.");
