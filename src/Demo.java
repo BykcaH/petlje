017274030c1584e61f3f3675e13b1a2c6d9d0b9f
CSU <CSU@CSU-TOSH>
2018-10-22 09:22:02 +0200

    Unos niza karaktera iz konzole, i provera da li dati element postoji u nizu elemenata.

--- src/Demo.java
+++ src/Demo.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		int [][] matrica = {{1, 2, 3}, {2, 3, 1}, {3, 2, 1}};
+		char[] nizKaraktera = new char[5];
+		System.out.println(nizKaraktera.length);
+		Scanner input = new Scanner(System.in);
 		
-		for(int i = 0; i < 3 ; i++) {
-			System.out.println(" ");
-			for(int j = 0; j<3; j++) {
-				System.out.print(matrica[i][j] );
-			}
+		System.out.println("Unesit 5 karaktera");
+		for(int i = 0; i < nizKaraktera.length ; i++) {
+			nizKaraktera[i] = input.nextLine().charAt(0);
+		}
+		
+		System.out.println("Unesite karakter za proveru");
+		char provera = input.nextLine().charAt(0);
+		
+		boolean nalaziSeUNizu = false;
+		for(int i = 0; i < nizKaraktera.length; i++) {
+			if(nizKaraktera[i] == provera) {
+				System.out.println("Char " + provera +
+						" se nalazi na " +  (i+1) + " poziciji");
+				nalaziSeUNizu = true;
+			} 
+		}
+		if(nalaziSeUNizu == false) {
+			System.out.println("Element se ne nalazi u nizu.");
 		}
 	}
 
